1f0b1b22328321689966606c29f781c6e8d9b2fe
Knots <weareknot@gmail.com>
2020-12-06 16:04:12 +0000

    feat[dal]: add "part" node type, more relationships
    [optional body]
    
    [optional footer(s)]

--- app/dal/nodes/system.py
+++ app/dal/nodes/system.py
@@ -1,9 +1,9 @@
-from neomodel import StructuredNode, StringProperty, RelationshipTo
-from .properties import ShortIdProperty
+from neomodel import StructuredNode, StringProperty, RelationshipTo, RelationshipFrom, One
 
 
 class System(StructuredNode):
     name = StringProperty(required=True, index=True)
     description = StringProperty(required=False)
 
+    user = RelationshipFrom('app.dal.nodes.user.User', 'OWNS', One)
     subsystems = RelationshipTo('System', 'SUBSYSTEM')
--- app/dal/nodes/user.py
+++ app/dal/nodes/user.py
@@ -7,5 +7,11 @@ class User(StructuredNode):
     gagl_id = ShortIdProperty()
     discord_id = StringProperty(required=False)
 
-    system = RelationshipTo(System, 'OWNS')
+    systems = RelationshipTo(System, 'OWNS')
+
+    @property
+    def all_systems(self):
+        q = 'MATCH (u:User) WHERE id(u)=$self MATCH (u)-[:OWNS]->(x)-[:SUBSYSTEM|MEMBER*0..]-(ss:System) RETURN ss'
+        results, columns = self.cypher(q)
+        return [System.inflate(row[-1]) for row in results]
 
